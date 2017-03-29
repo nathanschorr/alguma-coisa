8dcfbe0c281230b8c5fd374db2a4b22c55184d34
Marcelo <marcelo.bohn@consyste.com.br>
2017-03-28 21:29:09 -0300

    refatoração dos testes

--- calc.spec.js
+++ calc.spec.js
@@ -1,25 +1,23 @@
-const Code = require('code');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
+
+const { expect } = require('code');
 
 const { soma } = require('./calc');
 
 lab.test('somando números inteiros', (done) => {
-
-    Code.expect(soma(2, 3)).to.equal(5);
-    Code.expect(soma(20, 33)).to.equal(53);
+    expect(soma(2, 3)).to.equal(5);
+    expect(soma(20, 33)).to.equal(53);
     done();
 });
 
 lab.test('somando números negativos', (done) => {
-
-    Code.expect(soma(-2, -5)).to.equal(-7);
+    expect(soma(-2, -5)).to.equal(-7);
     done();
 });
 
 lab.test('não passando parâmetros', (done) => {
-
     const r = soma();
-    Code.expect(r.message).to.equal("parâmentos faltando")
+    expect(r.message).to.equal("parâmentos faltando")
     done();
 });
