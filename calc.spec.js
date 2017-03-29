aca8b06a2b3bd151d5c5cfdcd720d69221d5661c
Marcelo <marcelo.bohn@consyste.com.br>
2017-03-28 21:24:38 -0300

    inclusão de novos testes

--- calc.spec.js
+++ calc.spec.js
@@ -4,8 +4,22 @@ const lab = exports.lab = Lab.script();
 
 const { soma } = require('./calc');
 
-lab.test('retorno da soma de 2 e 3 deve ser 5', (done) => {
+lab.test('somando números inteiros', (done) => {
 
     Code.expect(soma(2, 3)).to.equal(5);
+    Code.expect(soma(20, 33)).to.equal(53);
+    done();
+});
+
+lab.test('somando números negativos', (done) => {
+
+    Code.expect(soma(-2, -5)).to.equal(-7);
+    done();
+});
+
+lab.test('não passando parâmetros', (done) => {
+
+    const r = soma();
+    Code.expect(r.message).to.equal("parâmentos faltando")
     done();
 });
